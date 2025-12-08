f2d5ff966b629c63cfbf2834a80ff009d2f919ed
Noah Cohn <noahc42@gmail.com>
2025-12-07 16:28:41 -0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
+  base: '/the-jokesters/',
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
